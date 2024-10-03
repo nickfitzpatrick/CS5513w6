3c722ef793d9c2a32283ba7ff7a76e381b0a049e
Nick Fitzpatrick <nickf5624@gmail.com>
2024-10-02 18:00:26 -0700

    two collections working

--- week6app/lib/data-firebase.js
+++ week6app/lib/data-firebase.js
@@ -1,4 +1,4 @@
-import app from "./firebase-app";
+ import app from "./firebase-app";
 import { getFirestore, collection, getDocs, doc, getDoc } from "firebase/firestore";
 
 const db = getFirestore(app);
@@ -32,6 +32,9 @@ export async function getSortedList(){
     );
 }
 
+
+
+
 // function returns ids for all json objects in array
 export async function getAllIds(){
     const snapshot = await getDocs( collection(db, "persons") );
@@ -55,6 +58,11 @@ export async function getAllIds(){
     );
 }
 
+
+
+
+
+
 // Function return all of the properties for a single object with matching id value
 export async function getData(idRequested) {
     const docRef = doc(db, "persons", idRequested);
@@ -65,6 +73,40 @@ export async function getData(idRequested) {
         objReturned = {};
     } else {
         objReturned = document.data();
+    }
+
+    const personalData = await getPersoanlData(objReturned.name);
+    objReturned = Object.assign(objReturned, personalData);
+
+    return objReturned;
+}
+
+
+
+export async function getPersoanlData(nameRequested){
+    const snapshot = await getDocs( collection(db, "personalInfo") );
+    const jsonObj = snapshot.docs.map(
+        ( document ) => (
+            {
+                id: document.id,
+                ...document.data() //apends other fields in array
+            }
+        )
+    );
+
+    // find object value in array that has matching name
+    const objMatch = jsonObj.filter(
+        function(obj) {
+            return obj.name === nameRequested;
+        }
+    );
+
+    // extract object value in filtered array if any
+    let objReturned;
+    if (objMatch.length > 0) {
+        objReturned = objMatch[0];
+    } else {
+        objReturned = {};
     }
 
     return objReturned;
